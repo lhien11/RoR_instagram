287f74a3f6dec345fee4a50957312c4341325fca
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-02-28 18:08:59 -0600

    finish template for now

--- Gemfile
+++ Gemfile
@@ -43,6 +43,9 @@ gem 'devise'
 gem 'paperclip'
 #masonry-rails
 gem 'masonry-rails', '~> 0.2.1'
+#acts-votable
+gem 'acts_as_votable', '~> 0.10.0'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,9 +1,14 @@
 class PicsController < ApplicationController
-  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
+  before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pics = Pic.all
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+  def show
+
   end
 
   def new
@@ -13,32 +18,36 @@ class PicsController < ApplicationController
   def create
     @pic = current_user.pics.build(pic_params)
     if @pic.save
-      redirect_to @pic, notice: "Yesss! It was posted!"
+      redirect_to @pic,notice: "Yesss! It was posted!"
     else
       render 'new'
     end
   end
 
-  def show
-  end
-
   def edit
+
   end
 
   def update
     if @pic.update(pic_params)
-      redirect_to @pic, notice: "Congrats! Pic was updated!"
+      redirect_to @pic, notice: "Congrats! Pic was updated"
     else
       render 'edit'
     end
   end
 
   def destroy
-    @pic.delete
-    redirect_to @pic
+    @pic.destroy
+    redirect_to root_path
+  end
+
+  def upvote
+    @pic.upvote_by current_user
+    redirect_to :back
   end
 
   private
+
   def pic_params
     params.require(:pic).permit(:title,:description, :image)
   end
